9761583e827c2c0c464a13e4cdae6eee2a8abb03
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-20 11:37:51 -0500

    initial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+
+        public static double AreaOfCircle(int radius)
+        {
+            double area;
+
+            area = Math.PI * (radius * radius);
+
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            double area;
+            Console.WriteLine("Give me the radius of a circle:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            area = AreaOfCircle(x);
+            Console.WriteLine("The area of this circle is:" + area);
+
+        }
+    }
+}
